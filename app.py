fc14df3bf01d2c6969bb6462651c55c2630bbfb8
HanyuXiao05 <hanyu.xiao@my.jcu.edu.au>
2024-08-10 16:23:51 +0800

    Add greet route and f route to convert temperature

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+@app.route('/f/<celsius>')
+def f(celsius=0):
+    return convert_temperature(celsius)
+
+
+def convert_temperature(celsius):
+    celsius = float(celsius)
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return f"{fahrenheit:.2f}"
+
 
 if __name__ == '__main__':
     app.run()
